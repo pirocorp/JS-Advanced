7ff99c31a48db0601cf238979bd47ca453c188c0
Zdravko Zdravkov <pirocorp@abv.bg>
2019-08-28 15:54:38 +0200

    08. Encode-and-Decode-Messages

---
+++ 02. DOM MANIPULATIONS/Exercise/08. Encode-and-Decode-Messages/app.js
@@ -0,0 +1,36 @@
+function encodeAndDecodeMessages() {
+    const buttons = document.getElementsByTagName('button');
+    const encodeButton = buttons[0];
+    const decodeButton = buttons[1];
+
+    function encodeMessage(e) {
+        const inputTextarea = e.currentTarget.parentElement.querySelector('textarea');
+        const outputTextarea = document.querySelector('textarea:disabled');
+        
+        const inputValue = inputTextarea.value;
+        let result = '';
+
+        for (let i = 0; i < inputValue.length; i++) {
+            result += String.fromCharCode(inputValue.charCodeAt(i) + 1);         
+        }
+
+        outputTextarea.value = result;
+        inputTextarea.value = '';
+    }
+
+    function decodeMessage(e) {
+        const textarea = e.currentTarget.parentElement.querySelector('textarea');
+        
+        const inputValue = textarea.value;
+        let result = '';
+
+        for (let i = 0; i < inputValue.length; i++) {
+            result += String.fromCharCode(inputValue.charCodeAt(i) - 1);
+        }
+
+        textarea.value = result;
+    }
+
+    encodeButton.addEventListener('click', encodeMessage);
+    decodeButton.addEventListener('click', decodeMessage);
+}
