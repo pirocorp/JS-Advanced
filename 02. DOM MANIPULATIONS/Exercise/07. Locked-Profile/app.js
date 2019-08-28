70434d5dd29094314f2d8650a3f470af982a8508
Zdravko Zdravkov <pirocorp@abv.bg>
2019-08-28 15:32:09 +0200

    07. Locked-Profile

---
+++ 02. DOM MANIPULATIONS/Exercise/07. Locked-Profile/app.js
@@ -0,0 +1,27 @@
+function lockedProfile() {
+    const buttons = document.getElementsByTagName('button');
+
+    function clickEventHandler(e) {
+        const currentButton = e.currentTarget;
+        const profileElement = currentButton.parentNode;
+        const lockedStatus = profileElement.querySelector('input[type=radio]:checked').value;
+
+        if (lockedStatus === 'lock') {
+            return;
+        } 
+
+        if (currentButton.textContent === 'Show more') {
+            const hiddenInformationElement = profileElement.querySelector('div');
+            hiddenInformationElement.style.display = 'block';
+            currentButton.textContent = 'Hide it';
+        } else {
+            const hiddenInformationElement = profileElement.querySelector('div');
+            hiddenInformationElement.style.display = 'none';
+            currentButton.textContent = 'Show more';
+        }
+    }
+    
+    for (const button of buttons) {
+        button.addEventListener('click', clickEventHandler);
+    }
+}
