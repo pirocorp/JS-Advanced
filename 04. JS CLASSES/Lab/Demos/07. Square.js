c175b9ac2e00d58c45085b3f0a97760144500adf
Zdravko Zdravkov <pirocorp@abv.bg>
2019-09-06 14:14:42 +0200

    Inheritance example updated

--- 04. JS CLASSES/Lab/Demos/07. Square.js
+++ 04. JS CLASSES/Lab/Demos/07. Square.js
@@ -29,3 +29,66 @@ class Square extends Rectangle {
         this.name = 'Square';
     }
 }
+
+//One may also extend traditional function-based "classes":
+function Animal(name) {
+    this.name = name;
+}
+
+Animal.prototype.speak = function () {
+    console.log(this.name + ' makes a noise.');
+}
+
+class Dog extends Animal {
+    speak() {
+        console.log(this.name + ' barks.');
+    }
+}
+
+let d = new Dog('Mitzie');
+d.speak(); // Mitzie barks.
+
+//Note that classes cannot extend regular (non-constructible) objects. 
+//If you want to inherit from a regular object, you can instead use 
+//Object.setPrototypeOf():
+const Animal = {
+    speak() {
+        console.log(this.name + ' makes a noise.');
+    }
+};
+
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+// If you do not do this you will get a TypeError when you invoke speak
+Object.setPrototypeOf(Dog.prototype, Animal);
+
+let d = new Dog('Mitzie');
+d.speak(); // Mitzie makes a noise.
+
+//The super keyword is used to call corresponding methods of super class. 
+//This is one advantage over prototype-based inheritance.
+class Cat {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        console.log(`${this.name} makes a noise.`);
+    }
+}
+
+class Lion extends Cat {
+    speak() {
+        super.speak();
+        console.log(`${this.name} roars.`);
+    }
+}
+
+let l = new Lion('Fuzzy');
+l.speak();
+// Fuzzy makes a noise.
+// Fuzzy roars.
