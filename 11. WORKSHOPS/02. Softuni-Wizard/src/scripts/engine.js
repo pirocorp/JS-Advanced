c8787f4833072deee5a632989aa45051645c49c9
Zdravko Zdravkov <zdr0@yahoo.com>
2019-09-20 15:34:22 +0300

    Implement next state functions

--- 11. WORKSHOPS/02. Softuni-Wizard/src/scripts/engine.js
+++ 11. WORKSHOPS/02. Softuni-Wizard/src/scripts/engine.js
@@ -19,13 +19,18 @@ const initialState = () => ({
     bugs: [],
 });
 
-//state will be immutable
+const nextPlayer = (state) => (state.player);
+const nextScene = (state) => (state.scene);
+const nextClouds = (state) => (state.clouds);
+const nextAttacks = (state) => (state.attacks);
+const nextBugs = (state) => (state.bugs);
+
 const next = (state) => ({
-    player: state.player,
-    scene: state.scene,
-    clouds: state.clouds,
-    attacks: state.attacks,
-    bugs: state.bugs
+    player: nextPlayer(state),
+    scene: nextScene(state),
+    clouds: nextClouds(state),
+    attacks: nextAttacks(state),
+    bugs: nextBugs(state)
 });
 
 function isCollision(firstElement, secondElement) {
