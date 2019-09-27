6384e940b27f4342e57c2107597b6522485d9ae9
Zdravko Zdravkov <pirocorp@abv.bg>
2019-09-27 12:44:00 +0200

    update

--- 13. Bonus INTRODUCTION TO AJAX/Lab/Demos/01.XMLHttpRequest/app.js
+++ 13. Bonus INTRODUCTION TO AJAX/Lab/Demos/01.XMLHttpRequest/app.js
@@ -1,6 +1,12 @@
 function loadRepos() {
     const req = new XMLHttpRequest();
 
+    //State  Description
+    //0      The request is not initialized
+    //1      The request has been set up
+    //2      The request has been sent
+    //3      The request is in process
+    //4      The request is complete
     req.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             document.getElementById('res').textContent = this.responseText;
