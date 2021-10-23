cd3e289bec318254e1ef624c5fade693588d260d
Zdravko Zdravkov <zdr0@yahoo.com>
2021-10-23 15:36:15 +0200

    Sudoku check

--- 11. WORKSHOPS/04. Sudoku/src/app.js
+++ 11. WORKSHOPS/04. Sudoku/src/app.js
@@ -4,13 +4,37 @@
 // Check solution
 
 import { p1 } from './puzzles.js';
-import { renderBoard } from './board.js';
+import { renderBoard, createButton } from './board.js';
 
 window.addEventListener('DOMContentLoaded', start);
 
 function start() {
     const main = document.querySelector('main');
+    const cells = renderBoard(p1, main);   
+    
+    const checkBtn = document.getElementById('checkBtn');
+    checkBtn.addEventListener('click', () => {
+        cells.blocks.forEach(check);
+        cells.rows.forEach(check);
+        cells.columns.forEach(check);
 
-    renderBoard(p1, main);
+        checkBtn.replaceWith(uncheckedBtn)
+    });
+
+    const uncheckedBtn = createButton('Clear Check', () => {
+        cells.blocks.forEach(c => c.forEach(x => x.classList.remove('error')));
+        uncheckedBtn.replaceWith(checkBtn);
+    });
 }
 
+function check(cells) {
+    const values = cells
+        .map(c => c.value)
+        .filter(c => c !== '')
+
+    const numbers = new Set(values);
+
+    if(numbers.size != 9) {
+        cells.forEach(c => c.classList.add('error'));
+    }
+}
--- 11. WORKSHOPS/04. Sudoku/src/board.js
+++ 11. WORKSHOPS/04. Sudoku/src/board.js
@@ -1,33 +1,62 @@
 export function renderBoard(values, main) {
-    main.appendChild(cluster(values[0]));
-    main.appendChild(cluster(values[1]));
-    main.appendChild(cluster(values[2]));
+    const clusters = values.map(cluster);
+    const blocks = [...clusters[0].cells, ...clusters[1].cells, ...clusters[2].cells];
+    const rows = [];
+    const columns = [];
+
+    main.appendChild(clusters[0].element);
+    main.appendChild(clusters[1].element);
+    main.appendChild(clusters[2].element);
+
+    formatCells(blocks, rows, columns);
+
+    return {
+        blocks,
+        rows,
+        columns
+    };
 }
 
 function cluster(values) {
+    const blocks = values.map(block);
+
     const element = createDOMElement(
         'div', 
         { className: 'cluster' }, 
-        ...values.map(b => block(b))
+        ...blocks.map(b => b.element)
     );
 
-    return element;
+    return {
+        cells: blocks.map(b => b.cells),
+        element,
+    };
 };
 
 function block (values) {
     const element = createDOMElement('div', {className: 'block'});
 
-    element.appendChild(row(values.slice(0, 3)));
-    element.appendChild(row(values.slice(3, 6)));
-    element.appendChild(row(values.slice(6)));
+    const row1 = row(values.slice(0, 3));
+    const row2 = row(values.slice(3, 6));
+    const row3 = row(values.slice(6));
 
-    return element;
+    element.appendChild(row1.element);
+    element.appendChild(row2.element);
+    element.appendChild(row3.element);
+
+    return {
+        cells: [...row1.cells, ...row2.cells, ...row3.cells],
+        element,        
+    };
 }; 
 
 function row (values) { 
-    const element = createDOMElement('div', {className: 'row'}, ...values.map(cell));
+    const cells = values.map(cell);
+    const element = createDOMElement('div', {className: 'row'}, ...cells);
 
-    return element;
+    return {
+        cells,
+        element
+    };
 };
 
 function cell(value) {
@@ -65,3 +94,41 @@ function createDOMElement(type, attr, ...content) {
 
     return element;
 };
+
+function formatCells(blocks, rows, columns) {
+    for (let matrixIndex = 0; matrixIndex < blocks.length; matrixIndex++) {
+        const block = blocks[matrixIndex];
+
+        for (let cellIndex = 0; cellIndex < block.length; cellIndex++) {
+            const { x, y } = calcCoordinates(matrixIndex, cellIndex);
+
+            if (rows[x] === undefined) {
+                rows[x] = [];
+            }
+
+            if (columns[y] === undefined) {
+                columns[y] = [];
+            }
+
+            rows[x][y] = block[cellIndex];
+            columns[y][x] = block[cellIndex];
+        }
+    }
+}
+
+function calcCoordinates(matrixIndex, cellIndex) {
+	const x = Math.floor(matrixIndex / 3) * 3  + Math.floor(cellIndex / 3);
+	const y = (matrixIndex % 3) * 3 + (cellIndex % 3);
+
+	return {
+        x,
+        y
+    }
+};
+
+export function createButton(label, callback) {
+    const element = createDOMElement('button', {}, label);
+    element.addEventListener('click', callback);
+
+    return element;
+}
